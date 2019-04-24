0342ed3bb9989eda33a65dbe0bc3a131e8ad9b72
chathula <schathula@gmail.com>
2019-04-24 14:03:51 +0530

    added unit testings of rendering components

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { MemoryRouter } from 'react-router-dom'
+import { Provider } from 'react-redux';
+import NavBar from './components/navBar';
+import LeaseView from './components/leaseView';
+import LeaseTable from './components/leaseTable';
+import Home from './components/pages/home';
+import Lease from './components/pages/lease';
+import store from './store';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('Check components rendering', () => {
+  it('renders NavBar without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<NavBar />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('renders LeaseView without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<MemoryRouter><LeaseView tenant='Tester' leaseId='lease-a' /></MemoryRouter>, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('renders LeaseTable without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<LeaseTable data={[]} />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('renders Home without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Provider store={store}><Home loading={true} /></Provider>, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('renders Lease without crashing', () => {
+    const div = document.createElement('div');
+    const defaultProps = {
+        match: { params: { code: 123 } },
+    };
+
+    ReactDOM.render(<Provider store={store}><Lease {...defaultProps} loading={true} /></Provider>, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
 });
